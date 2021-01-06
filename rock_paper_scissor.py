7bc3f9e494cfc8ba639de6cd4d76be1e690f1243
samiulislam09 <65601972+samiulislam09@users.noreply.github.com>
2021-01-06 23:51:10 +0600

    Update rock_paper_scissor.py

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -2,9 +2,39 @@ import random
 p = ['rock', 'paper', 'scissor']
 computer = p[random.randint(0, 2)]
 game_running = True
+
+
 while game_running:
     player = input('rock, paper, scissor?').lower()
-    print(f'Computer chosen {computer}')
+    print(f'--------Your choice {player}-------')
+    if player == 'rock':
+        print("""
+            _______
+        ---'   ____)
+              (_____)
+              (_____)
+              (____)
+        ---.__(___)
+        """)
+    elif player == 'paper':
+        print("""
+             _______
+        ---'    ____)____
+                   ______)
+                  _______)
+                 _______)
+        ---.__________)
+        """)
+    elif player == 'scissor':
+        print("""
+            _______
+        ---'   ____)____
+                  ______)
+               __________)
+              (____)
+        ---.__(___)
+        """)
+    print(f'-----Computer choice {computer} -----')
     if computer == 'rock':
         print("""
             _______
@@ -38,20 +68,23 @@ while game_running:
         if computer == 'scissor':
             print(f'you lose! {computer} cuts the {player}')
         else:
-            print(f'you won {player} covers {computer}')
+            print(f'Yay you won 😎 {player} covers {computer}')
     elif player == 'rock':
         if computer == 'paper':
             print(f'you lose {computer} covers {player}')
         else:
-            print(f'you won! {player} smashes {computer}')
+            print(f'Yay you won 😎 {player} smashes {computer}')
     elif player == 'scissor':
         if computer == 'rock':
             print(f'you lose! {computer} smashes {player}')
         else:
-            print(f'you won! {player} cut {computer}')
+            print(f'Yay you won 😎 {player} cut {computer}')
     else:
         print('this is not a valid play! try a valid keyword')
+    print('GΛMΣ-ӨVΣЯ')
 
     game_running = False
+
+
 
 
